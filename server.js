cc4c0096fe2749a43b8d94cbbec2ffed7d3496f4
Bhargav Devandla <bhargavdevandla2006@gmail.com>
2026-06-22 11:50:33 +0530

    update trips routes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 const db = new sqlite3.Database('./travel.db');
 
-const SECRET = "travel_secret_key" 
+const SECRET = "travel_secret_key"
 
 db.run(` create table if not exists users(
     id integer primary key autoincrement,
@@ -26,29 +26,27 @@ db.run(` create table if not exists trips (
     location TEXT,
     price TEXT,
     image TEXT
-    )`)    
+    )`)
 
-app.get('/trips',  async(req, res) =>{
+app.get('/trips', async (req,res)=>{
+ db.all(
+    "select * from trips",
+    [],
+    (err, rows) =>{
+       if (err) {
+    console.log("SQLite Error:", err);
 
-    try{
-
-       const response = await fetch("https://restcountries.com/v3.1/all");
-       const countries =  await response.json();
-       
-       const trips = countries.slice(0,12).map((country, index) => ({
-         id:index+1,
-         title:country.name.common,
-         location:country.region,
-         price:`₹${(index + 1) * 10000}`,
-         image: country.flags.png
-       }))
-       res.json(trips)
-    }catch(error){
-        res.status(500).json({
-            message:"failed to fetch trips data"
-        })
+    return res.status(500).json({
+        message: "Failed to fetch trips",
+        error: err.message
+    });
+}
+        res.json(rows)
     }
+ )
 })
+
+
 app.post("/register", async (req, res) => {
 
     const { name, email, password } = req.body;
@@ -84,46 +82,71 @@ app.post("/register", async (req, res) => {
     );
 });
 
-app.post("/login", async (req, res) =>{
-    const {email, password} = req.body;
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
 
     db.get(
         "select * from users where email=?",
         [email],
 
-        async(err, user) =>{
-            if(!user){
+        async (err, user) => {
+            if (!user) {
                 return res.status(400).json({
-                    message:"Invalid Email"
+                    message: "Invalid Email"
                 })
             }
             const isMatch = await bcrypt.compare(
                 password,
                 user.password
             );
-            if(!isMatch){
+            if (!isMatch) {
 
                 return res.status(400).json({
-                    message:"Invalid password"
+                    message: "Invalid password"
                 })
             }
-            const token  = jwt.sign(
+            const token = jwt.sign(
                 {
-                id:user.id
-            },
-            SECRET,
-            {
-                expiresIn :"7d"
-            }
-        );
-        res.json({
-            token
-        })
+                    id: user.id
+                },
+                SECRET,
+                {
+                    expiresIn: "7d"
+                }
+            );
+            res.json({
+                token
+            })
         }
     )
 })
 
-app.get("/" , async(req, res) =>{
+app.post('/trips', async (req, res) => {
+    const { title, location, price, image } = req.body;
+
+db.run(
+    `
+    INSERT INTO trips(title, location, price, image)
+    VALUES (?, ?, ?, ?)
+    `,
+    [title, location, price, image],
+    function (err) {
+        if (err) {
+            return res.status(500).json({
+                message: "Failed to add trip"
+            });
+        }
+
+        res.json({
+            id: this.lastID,
+            message: "Trip added successfully"
+        });
+    }
+);
+})
+
+
+app.get("/", async (req, res) => {
     res.send("Backend is running bruuuuu")
 })
 
